36a66c4fd328cfb79ff0a12f4d09db2de78cc67b
Tien Dao <tiendao5240@gmail.com>
2022-05-26 14:58:00 -0400

    Added class UICollectionViewController

---
+++ DemoURLsessions/PhotosCollectionCollectionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PhotosCollectionCollectionViewController.swift
+//  DemoURLsessions
+//
+//  Created by Tien Dao on 2022-05-26.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class PhotosCollectionCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
+
+
+
